79b8e22037935760a71cd4207f38c4cd5bf611b5
Adan <adanshahid.student@gmail.com>
2025-06-06 11:11:27 +0500

    This time successfully implemented the static files as well.

--- blogwebsite_main/settings.py
+++ blogwebsite_main/settings.py
@@ -117,6 +117,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+#FOR CONFIGURING STATIC FILES
+STATIC_ROOT = BASE_DIR /'static'
+STATICFILES_DIRS = [
+    'blogwebsite_main/static'
+]
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
