2061df09ff63c53286052ef0b93089a0aee77db2
Adan Shahid <adanshahid.student@gmail.com>
2026-01-12 17:52:43 +0500

    updated Blog/models.py

--- Blog/admin.py
+++ Blog/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import Post
 # Register your models here.
-admin.site.register(Post)
+@admin.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'author', 'publish']
+    list_filter = ['status', 'created', 'publish', 'author']
+    search_fields = ['title', 'body']
+    prepopulated_fields = {'slug':('title',)}
+    raw_id_fields =['author']
+    ordering = ['status', 'publish']
