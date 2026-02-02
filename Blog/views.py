e37773b90894121356d150ff5d225025d0043e94
adan-shahid <adanshahid.student@gmail.com>
2026-02-02 21:45:57 +0500

    handled some edge cases in pagination, now pagination is complete

--- Blog/views.py
+++ Blog/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from .models import Post
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 def post_list(request):
     post_list = Post.published.all()
     paginator = Paginator(post_list, 2) # 3 posts per page
     page_number = request.GET.get('page', 1)
-    posts = paginator.page(page_number)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
     return render(request, 'blog/post/list.html', {'posts':posts})
 
 def post_detail(request, year, month, day, post):
