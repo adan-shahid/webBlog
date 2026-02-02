e32fd4108ad6501a57f35f378ee23903a1952fc3
adan-shahid <adanshahid.student@gmail.com>
2026-02-02 21:41:10 +0500

    Added the Pagination, next we will handle some pagination errors

--- Blog/views.py
+++ Blog/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from .models import Post
+from django.core.paginator import Paginator
 
 # Create your views here.
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    paginator = Paginator(post_list, 2) # 3 posts per page
+    page_number = request.GET.get('page', 1)
+    posts = paginator.page(page_number)
     return render(request, 'blog/post/list.html', {'posts':posts})
 
 def post_detail(request, year, month, day, post):
@@ -14,6 +18,7 @@ def post_detail(request, year, month, day, post):
                             publish__month=month,
                             publish__day=day,
                             status=Post.Status.PUBLISHED)
+
     return render(request,
                   'blog/post/detail.html',
                   {'post':post})
