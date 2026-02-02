2566ae4666a90eb246a16eddad8e0db1e348a6af
adan-shahid <adanshahid.student@gmail.com>
2026-02-02 21:20:30 +0500

    canonical urls for seo added

--- Blog/views.py
+++ Blog/views.py
@@ -7,9 +7,12 @@ def post_list(request):
     posts = Post.published.all()
     return render(request, 'blog/post/list.html', {'posts':posts})
 
-def post_detail(request, id):
+def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post,
-                            id=id,
+                            slug=post,
+                            publish__year=year,
+                            publish__month=month,
+                            publish__day=day,
                             status=Post.Status.PUBLISHED)
     return render(request,
                   'blog/post/detail.html',
